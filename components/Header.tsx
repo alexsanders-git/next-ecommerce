692413d3f8da8293c0e37f336fe6a3e1f45711ae
Alex Sanders <alex.sanders@ukr.net>
2023-06-04 14:41:15 +0300

    Fixed style

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
 				/>
 			</Link>
 
-			<div className="flex items-center space-x-2.5 text-sm">
+			<div className="flex items-center flex-col sm:flex-row gap-2 space-x-2.5 text-sm">
 				<button className="px-6 py-2 text-sm text-indigo-500 transition-colors duration-300 border-2 border-indigo-400 rounded-full shadow-xl shadow-indigo-300/30 hover:bg-indigo-500 hover:text-indigo-100">Log in</button>
 				<button className="px-6 py-2 text-sm text-indigo-500 transition-colors duration-300 border-2 border-indigo-400 rounded-full shadow-xl shadow-indigo-300/30 hover:bg-indigo-500 hover:text-indigo-100">Sign Up</button>
 			</div>
--- components/Product.tsx
+++ components/Product.tsx
@@ -10,15 +10,15 @@ const Product = ({ product }: Props) => {
 		<Link
 			prefetch={false}
 			href={`/product/${product.id}`}
-			className="h-96 flex flex-col border group-hover:scale-105 transition-transform ease-out duration-200"
+			className="h-96 flex flex-col border group-hover:scale-105 transition-transform ease-out duration-200 p-4"
 		>
 			<div className="relative max-h-72 flex-1">
 				<ProductImage product={product} fill />
 			</div>
 
-			<div className="font-semibold flex items-center justify-between mt-4 mb-1">
-				<p>{product.title}</p>
-				<p>${product.price}</p>
+			<div className="font-semibold flex flex-col items-center justify-between mt-4 mb-1">
+				<p className="mb-3">{product.title}</p>
+				<p className="mb-3">${product.price}</p>
 				<p className="italic test-xs w-64 line-clamp-2 text-gray-600">{product.description}</p>
 			</div>
 		</Link>
